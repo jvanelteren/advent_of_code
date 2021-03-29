c378dd6adfb8e2fe49789745ac2fc5f61c1c498a
Jesse van Elteren <jvanelteren@gmail.com>
2021-03-29 08:23:00 +0200

    reduced further

--- synacor/test.py
+++ synacor/test.py
@@ -1,15 +1,17 @@
 #%%
-#  4, 6056, 6047, 3, 6056, 3, 6056]
-magic = 30000
+# 4, 6056, 6047, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056]
+# 
+magic = 20
 reg = {0:3,1:magic, 7:magic}
+reg = {0:4,1:1, 7:magic}
 stack = []
 def run():
     i = 0
-    while i<1000000:
-        
+    while i<10000000000:
+        # print(stack)
+
         i+=1
-        if i % 100000 == 0:
-            print(len(stack))
+
         if reg[0] != 0: #center 
             if reg[1] != 0:
                 #jmp
@@ -23,13 +25,14 @@ def run():
                 stack.append(6047)
                 continue
         else: #6027 reg0==0
-            print(i)
+            print(reg)
+            # print(i)
             reg[0] = reg[1]+1
             if not stack:
                 print('done, reg[0] should now be 6')
             # ret
             while stack[-1] != 6056:
-                if stack[-1] == 6047: 
+                if stack[-1] == 6047 or stack[-1] == 6067 : 
                     stack.pop()
                 else:
                     print(stack[-1],'what to do')
@@ -38,9 +41,55 @@ def run():
             reg[1]=reg[0]
             reg[0]=stack.pop()
             reg[0]-= 1
-            stack.append(6067)
+            stack.append(6067) #deze call weet ik niet zeker! is wel altijd zelfde waarde
+            print(stack[-5:], reg, len(stack))
+            
+            # print(reg[0], reg[1], len(stack))
             continue
 run()
+# stack[:100]
+
 # %%
 1+1
+# %%
+#%%
+# 4, 6056, 6047, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056, 3, 6056]
+# 
+magic = 20
+reg = {0:3,1:magic, 7:magic}
+reg = {0:4,1:1, 7:magic}
+stack = []
+def run():
+    i = 0
+    while i<10000000000000:
+        if i % 1000000 ==0 : print(len(stack))
+
+        i+=1
+
+        if reg[0] != 0: #center 
+            if reg[1] != 0:
+                #jmp
+                stack.append(reg[0])
+                reg[1] -= 1
+                continue
+            else:
+                reg[0] -= 1
+                reg[1] = reg[7]
+                continue
+        else: #6027 reg0==0
+            # print(reg)
+            # print(i)
+            reg[0] = reg[1]+1
+            if not stack:
+                print('done, reg[0] should now be 6')
+        
+            reg[1]=reg[0]
+            reg[0]=stack.pop()
+            reg[0]-= 1
+            # print(stack[-5:], reg, len(stack))
+            
+            # print(reg[0], reg[1], len(stack))
+            continue
+run()
+# stack[:100]
 # %%
